8c65e774d1132d1eac5d90ed975dd14a4dc87586
James <james.thomas.vaughn@gmail.com>
2017-10-19 16:46:53 -0400

    Have static resources loading properly in both jar and local

--- src/main/java/webapp/Main.java
+++ src/main/java/webapp/Main.java
@@ -8,8 +8,10 @@ import static spark.Spark.*;
 public class Main {
     public static void main(String[] args) {
         port(8000);
+        staticFiles.location("/public");
+
         get("/", (request, response) -> {
-            return new MustacheTemplateEngine().render(new ModelAndView(new HashMap<String, Object>(), "resources/index.html"));
+            return new MustacheTemplateEngine().render(new ModelAndView(new HashMap<String, Object>(), "public/index.html"));
         });
     }
 }
