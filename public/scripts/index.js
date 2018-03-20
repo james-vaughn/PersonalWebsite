4e42384da01c1f2ea5f034691e8688c9e8bd645c
James Vaughn <james.thomas.vaughn@gmail.com>
2018-03-20 12:47:49 -0400

    Small refactor

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     const submitFileInput = $("#submitfile");
     const originalId = $("#original");
+    const newId = $("#new");
 
     //see https://www.nczonline.net/blog/2012/05/15/working-with-files-in-javascript-part-2/
     submitFileInput.on("change", function() {
@@ -15,26 +16,35 @@ $(document).ready(function () {
                 const img = new Image();
                 img.onload = function() {
                     drawImageScaled(img, originalContext);
+
+                    createMarkovImage(originalContext, newId.getContext("2d"));
                 };
 
                 img.src = dataUri;
             };
+
             reader.onerror = function(event) {
                 console.error("File could not be read! Code " + event.target.error.code);
             };
             reader.readAsDataURL(file);
         }
     });
+
+    function drawImageScaled(img, ctx) {
+        var canvas = ctx.canvas ;
+        var hRatio = canvas.width  / img.width    ;
+        var vRatio =  canvas.height / img.height  ;
+        var ratio  = Math.min ( hRatio, vRatio );
+        var centerShift_x = ( canvas.width - img.width*ratio ) / 2;
+        var centerShift_y = ( canvas.height - img.height*ratio ) / 2;
+        ctx.clearRect(0,0,canvas.width, canvas.height);
+        ctx.drawImage(img, 0,0, img.width, img.height,
+            centerShift_x,centerShift_y,img.width*ratio, img.height*ratio);
+    }
+
+    function createMarkovImage(oldContext, newContext) {
+        
+    }
 });
 
-function drawImageScaled(img, ctx) {
-    var canvas = ctx.canvas ;
-    var hRatio = canvas.width  / img.width    ;
-    var vRatio =  canvas.height / img.height  ;
-    var ratio  = Math.min ( hRatio, vRatio );
-    var centerShift_x = ( canvas.width - img.width*ratio ) / 2;
-    var centerShift_y = ( canvas.height - img.height*ratio ) / 2;
-    ctx.clearRect(0,0,canvas.width, canvas.height);
-    ctx.drawImage(img, 0,0, img.width, img.height,
-        centerShift_x,centerShift_y,img.width*ratio, img.height*ratio);
-}
+
