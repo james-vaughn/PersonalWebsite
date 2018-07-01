390e83670e6e216849bc34605b6f30bbb2e4542c
James Vaughn <james.thomas.vaughn@gmail.com>
2018-07-01 11:51:13 -0400

    Have templates working

--- Controllers/Home.go
+++ Controllers/Home.go
@@ -6,8 +6,33 @@ import (
 )
 
 
+func RegisterRoutes(r *gin.Engine) {
+	r.GET("/", Index)
+	r.GET("/termites", Termites)
+	r.GET("/particles", Particles)
+	r.GET("/markovchains", MarkovChains)
+}
+
 func Index(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.tmpl", gin.H {
 		"title" : "Welcome",
+	})
+}
+
+func Termites(c *gin.Context) {
+	c.HTML(http.StatusOK, "termites.tmpl", gin.H {
+		"title" : "Termites",
+	})
+}
+
+func Particles(c *gin.Context) {
+	c.HTML(http.StatusOK, "particles.tmpl", gin.H {
+		"title" : "Particles",
+	})
+}
+
+func MarkovChains(c *gin.Context) {
+	c.HTML(http.StatusOK, "markovChains.tmpl", gin.H {
+		"title" : "Markov Chains",
 	})
 }
--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"./Controllers"
+	"github.com/james-vaughn/PersonalWebsite/Controllers"
 )
 
 func main() {
@@ -13,7 +13,7 @@ func main() {
 	r.Use(gin.Recovery())
 	r.LoadHTMLGlob("Views/*")
 
-	r.GET("/", Controllers.Index)
+	Controllers.RegisterRoutes(r)
 
 	r.Run(PORT)
 }
