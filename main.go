b3f0a3009f0b42d2c75384237b7405f1c6f471a5
James <james.thomas.vaughn@gmail.com>
2018-08-15 21:25:08 -0400

    Templates

--- main.go
+++ main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	//"github.com/gin-contrib/static"
 	"github.com/james-vaughn/PersonalWebsite/Controllers"
+	"github.com/gin-contrib/multitemplate"
+	"path/filepath"
 )
 
 func main() {
@@ -12,11 +13,33 @@ func main() {
 	r := gin.Default()
 
 	r.Use(gin.Recovery())
-	//r.Use(static.Serve("/Public", static.LocalFile("/Public", false)))
-	r.LoadHTMLGlob("Views/*")
+	r.LoadHTMLGlob("Views/*/*")
+	r.HTMLRender = loadTemplates("./Views")
 	r.Static("/Public", "./Public")
 
 	Controllers.RegisterRoutes(r)
 
 	r.Run(PORT)
+}
+
+func loadTemplates(templatesDir string) multitemplate.Renderer {
+	r := multitemplate.NewRenderer()
+
+	layouts, err := filepath.Glob(templatesDir + "/Layouts/*.tmpl")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	includes, err := filepath.Glob(templatesDir + "/Includes/*.tmpl")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Generate our templates map from our layouts/ and includes/ directories
+	for _, include := range includes {
+		//child template must come first, then the base layouts
+		files := append([]string{include}, layouts...)
+		r.AddFromFiles(filepath.Base(include), files...)
+	}
+	return r
 }
