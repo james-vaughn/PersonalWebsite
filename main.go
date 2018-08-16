3c56c9c9ae40bdd420cfa96b93519d925647d29a
James <james.thomas.vaughn@gmail.com>
2018-08-15 20:18:37 -0400

    Have termite page added with styles

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	//"github.com/gin-contrib/static"
 	"github.com/james-vaughn/PersonalWebsite/Controllers"
 )
 
@@ -11,7 +12,9 @@ func main() {
 	r := gin.Default()
 
 	r.Use(gin.Recovery())
+	//r.Use(static.Serve("/Public", static.LocalFile("/Public", false)))
 	r.LoadHTMLGlob("Views/*")
+	r.Static("/Public", "./Public")
 
 	Controllers.RegisterRoutes(r)
 
