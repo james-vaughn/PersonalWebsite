cb55c81a981b635895af1c47e192082e8bcf26b9
James <james.thomas.vaughn@gmail.com>
2018-08-22 19:57:57 -0400

    Added menu dropdown

--- Public/Scripts/index.js
+++ Public/Scripts/index.js
@@ -1,5 +1,22 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    document.getElementById("menu").onclick = function(event) {
-        alert("Test");
+    document.getElementById("menu-icon").onclick = function(event) {
+        toggle(document.getElementsByClassName("dropdown-content").item(0));
     };
 });
+
+function show(elem) {
+    elem.style.display = 'block';
+}
+
+function hide(elem) {
+    elem.style.display = 'none';
+}
+
+function toggle(elem) {
+    if (window.getComputedStyle(elem).display === 'block') {
+        hide(elem);
+        return;
+    }
+
+    show(elem);
+}
