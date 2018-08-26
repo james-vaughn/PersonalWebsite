54f2e6efc4d78dc1e96b3cda828129074a79f7ef
James <james.thomas.vaughn@gmail.com>
2018-08-25 21:56:36 -0400

    Add the SEIR content

--- Controllers/Projects.go
+++ Controllers/Projects.go
@@ -1,13 +1,14 @@
 package Controllers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/james-vaughn/PersonalWebsite/Models"
 	"github.com/james-vaughn/PersonalWebsite/Services"
-	"net/http"
 )
 
-type ProjectsController struct{
+type ProjectsController struct {
 	PagesService *Services.PagesService
 	pages        []Models.Page
 }
@@ -17,12 +18,12 @@ const ControllerName = "projects"
 func NewProjectsController(pagesService *Services.PagesService) *ProjectsController {
 	return &ProjectsController{
 		PagesService: pagesService,
-		pages :       pagesService.GetPagesFor(ControllerName),
+		pages:        pagesService.GetPagesFor(ControllerName),
 	}
 }
 
 func (c *ProjectsController) RegisterRoutes(r *gin.Engine) {
-	r.GET("/"+ControllerName, c.Index)
+	r.GET("/"+ControllerName+"/", c.Index)
 
 	for _, page := range c.pages {
 		url := c.PagesService.GetUrlFor(page)
@@ -32,23 +33,23 @@ func (c *ProjectsController) RegisterRoutes(r *gin.Engine) {
 
 func (c *ProjectsController) Index(context *gin.Context) {
 
-	context.HTML(http.StatusOK, "projectsindex.tmpl", gin.H {
-		"title" : "Projects",
-		"projects" : c.pages,
+	context.HTML(http.StatusOK, "projectsindex.tmpl", gin.H{
+		"title":    "Projects",
+		"projects": c.pages,
 	})
 }
 
-func (c *ProjectsController) ProjectPage(page Models.Page) func(*gin.Context){
+func (c *ProjectsController) ProjectPage(page Models.Page) func(*gin.Context) {
 	return func(context *gin.Context) {
 		prevPage := c.PagesService.GetPageFromList(c.pages, page.PrevId)
 		nextPage := c.PagesService.GetPageFromList(c.pages, page.NextId)
 
-		context.HTML(http.StatusOK, page.Url+".tmpl", gin.H {
-			"title" : page.Title,
-			"prev_url" : c.PagesService.GetUrlFor(prevPage),
-			"prev" : prevPage.Title,
-			"next_url" : c.PagesService.GetUrlFor(nextPage),
-			"next" : nextPage.Title,
+		context.HTML(http.StatusOK, page.Url+".tmpl", gin.H{
+			"title":    page.Title,
+			"prev_url": c.PagesService.GetUrlFor(prevPage),
+			"prev":     prevPage.Title,
+			"next_url": c.PagesService.GetUrlFor(nextPage),
+			"next":     nextPage.Title,
 		})
 	}
 }
